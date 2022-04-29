cfaed766ac9a8b32f6d75e3fe77133840df930bf
Paul Denning <pauld@mitre.org>
2022-04-29 16:38:48 -0400

    Hello World CQL and 2 VTE test cases

--- _extractMATBundle.bat
+++ _extractMATBundle.bat
@@ -1,18 +1,32 @@
 @ECHO OFF
-SET tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
-SET input_cache_path=%~dp0input-cache
-SET mat_bundle=bundles\mat\CLONE124_v6_03-Artifacts\measure-json-bundle.json
+IF DEFINED CQF-TOOLING-HOME (
+	ECHO Looking in %CQF-TOOLONG-HOME% for jar file
+
+) else (
+
+ECHO Please set CQF-TOOLING-HOME environment variable
+GOTO done
+)
+if "%1"==""  GOTO usage
+SET mat_bundle=%1
+if NOT EXIST "%mat_bundle%" (GOTO nobundle)
+
+SET tooling_jar=tooling-1.4.1-SNAPSHOT-jar-with-dependencies.jar
 
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
-IF EXIST "%input_cache_path%\%tooling_jar%" (
-	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -ExtractMatBundle %mat_bundle%
-	JAVA -jar "%input_cache_path%\%tooling_jar%" -ExtractMatBundle %mat_bundle%
-) ELSE If exist "..\%tooling_jar%" (
-	ECHO running: JAVA -jar "..\%tooling_jar%" -ExtractMatBundle %mat_bundle%
-	JAVA -jar "..\%tooling_jar%" -ExtractMatBundle %mat_bundle%
-) ELSE (
-	ECHO Tooling JAR NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
-)
+IF EXIST "%CQF-TOOLING-HOME%\%tooling_jar%" (
+	ECHO running: JAVA -jar "%CQF-TOOLING-HOME%\%tooling_jar%" -ExtractMatBundle %mat_bundle%
+	JAVA -jar "%CQF-TOOLING-HOME%\%tooling_jar%" -ExtractMatBundle %mat_bundle%
 
+) ELSE (
+	ECHO Tooling JAR NOT FOUND in %CQF-TOOLING-HOME%.  Please run _updateCQFTooling.  Aborting...
+)
+GOTO done
+:nobundle
+echo %mat_bundle% does not exist. See below for posible json files
+dir /b/s/a-d . | grep bundles.mat | grep \.json
+:usage
+ECHO Usage: _extractMATBundle jsonFile // e.g. jsonFile = bundles\mat\0512\CMS104-v1-0-FHIR-4-0-1.json 
+:done
 PAUSE
--- _updateCQFTooling.bat
+++ _updateCQFTooling.bat
@@ -1,86 +1,37 @@
 @ECHO OFF
+IF DEFINED CQF-TOOLING-HOME (
+	ECHO Looking in %CQF-TOOLONG-HOME% for jar file
 
-SET "dlurl=https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=org.opencds.cqf&a=tooling&v=1.4.1-SNAPSHOT&c=jar-with-dependencies"
+) else (
+
+ECHO Please set CQF-TOOLING-HOME environment variable
+GOTO done
+)
+SET "dlurl=https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots^&g=org.opencds.cqf^&a=tooling^&v=1.4.1-SNAPSHOT^&c=jar-with-dependencies"
 SET tooling_jar=tooling-1.4.1-SNAPSHOT-jar-with-dependencies.jar
-SET input_cache_path=%~dp0input-cache\
+REM SET input_cache_path=%~dp0input-cache\
+SET input_cache_path=%CQF-TOOLING-HOME%\
 SET skipPrompts=false
 IF "%~1"=="/f" SET skipPrompts=true
 
 FOR %%x IN ("%CD%") DO SET upper_path=%%~dpx
 
-IF NOT EXIST "%input_cache_path%%tooling_jar%" (
-   IF NOT EXIST "%upper_path%%tooling_jar%" (
-      SET jarlocation=%input_cache_path%%tooling_jar%
-      SET jarlocationname=Input Cache
-      ECHO IG Refresh is not yet in input-cache or parent folder.
-      REM we don't use jarlocation below because it will be empty because we're in a bracketed if statement
-      GOTO create
-   ) ELSE (
-      ECHO IG RefreshFOUND in parent folder
-      SET jarlocation=%upper_path%%tooling_jar%
-      SET jarlocationname=Parent folder
-      GOTO:upgrade
-   )
+IF NOT EXIST "%CQF-TOOLING-HOME%\%tooling_jar%" (
+REM Download the jar
 ) ELSE (
-   ECHO IG Refresh FOUND in input-cache
-   SET jarlocation=%input_cache_path%%tooling_jar%
-   SET jarlocationname=Input Cache
-   GOTO:upgrade
+   ECHO %tooling_jar% FOUND in %CQF-TOOLING-HOME%
+
+   GOTO:done
 )
-
-:create
-ECHO Will place refresh jar here: %input_cache_path%%tooling_jar%
-IF "%skipPrompts%"=="false" (
-    SET /p create=Ok? [Y/N]
-    IF /I "%create%"=="Y" goto:mkdir
-) ELSE goto:mkdir
-
-GOTO:done
-:mkdir
-    mkdir "%input_cache_path%" 2> NUL
-GOTO:download
-
-:upgrade
-IF "%skipPrompts%"=="false" (
-    SET /p overwrite="Overwrite %jarlocation%? (Y/N)"
-    IF /I "%overwrite%"=="Y" (
-        GOTO:download
-    )
-) ELSE (
-    GOTO:download
-)
-GOTO:done
 
 :download
-ECHO Downloading most recent refresh to %jarlocationname% - it's ~70 MB, so this may take a bit
-
-FOR /f "tokens=4-5 delims=. " %%i IN ('ver') DO SET VERSION=%%i.%%j
-IF "%version%" == "10.0" GOTO win10
-IF "%version%" == "6.3" GOTO win8.1
-IF "%version%" == "6.2" GOTO win8
-IF "%version%" == "6.1" GOTO win7
-IF "%version%" == "6.0" GOTO vista
-
-ECHO Unrecognized version: %version%
-GOTO done
-
-:win10
-POWERSHELL -command "if ('System.Net.WebClient' -as [type]) {(new-object System.Net.WebClient).DownloadFile('%dlurl%','%jarlocation%') } else { Invoke-WebRequest -Uri '%dlurl%' -Outfile '%jarlocation%' }"
-ECHO Download complete.
-GOTO done
-
-:win7
-bitsadmin /transfer GetRefresh /download /priority normal "%dlurl%" "%jarlocation%"
-ECHO Download complete.
-GOTO done
-
-:win8.1
-:win8
-:vista
-ECHO This script does not yet support Windows %winver%.  Please ask for help on https://chat.fhir.org/#narrow/stream/179207-connectathon-mgmt/topic/Clinical.20Reasoning.20Track
-GOTO done
+ECHO Downloading from %dlurl%
+ECHO Downloading %tooling_jar% to %CQF-TOOLING-HOME% - it's large, so this may take a bit ...
+REM -L means to folow redirects
+curl %dlurl% -L --output-dir %CQF-TOOLING-HOME% -o %tooling_jar%
 
 :done
 IF "%skipPrompts%"=="false" (
     PAUSE
 )
+@ECHO ON
